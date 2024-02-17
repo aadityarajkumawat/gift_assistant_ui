f0335ad89f09de2277916a179334d4b731ae4995
Aditya Raj Kumawat <arkumawat78@gmail.com>
2024-02-16 19:03:50 -0500

    added all

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,6 +4,7 @@ import OpenAI from 'openai'
 
 import { auth } from '@/auth'
 import { nanoid } from '@/lib/utils'
+import { NextResponse } from 'next/server'
 
 export const runtime = 'edge'
 
@@ -13,53 +14,71 @@ const openai = new OpenAI({
 
 export async function POST(req: Request) {
   const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+  const { messages, previewToken, user } = json
 
-  if (!userId) {
-    return new Response('Unauthorized', {
-      status: 401
-    })
-  }
+  console.log('messages', messages)
 
-  if (previewToken) {
-    openai.apiKey = previewToken
-  }
+  const lastMsg = messages[messages.length - 1]
 
-  const res = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
+  const responseFromAPI = await fetch('http://localhost:5001/chat', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ user: lastMsg.content })
   })
 
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId,
-        createdAt,
-        path,
-        messages: [
-          ...messages,
-          {
-            content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
-  })
+  const body = await responseFromAPI.json()
 
-  return new StreamingTextResponse(stream)
+  // console.log(body)
+
+  // const userId = (await auth())?.user.id
+
+  // if (!userId) {
+  //   return new Response('Unauthorized', {
+  //     status: 401
+  //   })
+  // }
+
+  // if (previewToken) {
+  //   openai.apiKey = previewToken
+  // }
+
+  // const res = await openai.chat.completions.create({
+  //   model: 'gpt-3.5-turbo',
+  //   messages,
+  //   temperature: 0.7,
+  //   stream: true
+  // })
+
+  // const stream = OpenAIStream(res, {
+  //   async onCompletion(completion) {
+  //     const title = json.messages[0].content.substring(0, 100)
+  //     const id = json.id ?? nanoid()
+  //     const createdAt = Date.now()
+  //     const path = `/chat/${id}`
+  //     const payload = {
+  //       id,
+  //       title,
+  //       userId,
+  //       createdAt,
+  //       path,
+  //       messages: [
+  //         ...messages,
+  //         {
+  //           content: completion,
+  //           role: 'assistant'
+  //         }
+  //       ]
+  //     }
+  //     await kv.hmset(`chat:${id}`, payload)
+  //     await kv.zadd(`user:chat:${userId}`, {
+  //       score: createdAt,
+  //       member: `chat:${id}`
+  //     })
+  //   }
+  // })
+
+  // return { response: body }
+  return NextResponse.json({ body })
 }
--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -15,7 +15,60 @@ export interface ChatMessageProps {
   message: Message
 }
 
+const PRODUCTS = [
+  {
+    id: 1,
+    name: 'Nutty & Nourishing Shea Big Gift',
+    price: 22,
+    brand_name: 'The Body Shop Home',
+    brand_url: 'thebodyshop.com',
+    product_vertical: 'Miscellaneous',
+    product_page_url:
+      'https://www.thebodyshop.com/en-us/sale/view-all-sale/nutty-nourishing-shea-big-gift/p/p173017',
+    currency: 'USD',
+    currency_raw: '(#1039336)',
+    sub_category: '(#1039336)',
+    style_and_material: '(#1039336)',
+    product_image:
+      '{"url":"https://media.thebodyshop.com/i/thebodyshop/1039336_G4_MEDIUM_SHEA_XM23_A0X_BRONZE_NW_INAFVPS042?$product-zoom$&fmt=auto"}'
+  },
+  {
+    id: 2,
+    name: "Gift Set - Because You're Amazing",
+    availability: 'InStock',
+    price: 65,
+    brand_name: 'Gift Sets',
+    brand_url: 'thehandmadesoapcompany.com',
+    product_vertical: 'Miscellaneous',
+    product_page_url:
+      'https://www.thehandmadesoapcompany.com/gift-set-because-youre-amazing-703g/13189923.html',
+    currency: 'USD',
+    currency_raw: 13189923,
+    sub_category: 13189923,
+    style_and_material: 13189923,
+    product_image:
+      '{"url":"https://static.thcdn.com/images/large/webp//productimg/1600/1600/13189923-1145084384609286.jpg"}'
+  }
+]
+
 export function ChatMessage({ message, ...props }: ChatMessageProps) {
+  console.log('CONTENT', message.content)
+
+  const products =
+    message.role !== 'user' ? JSON.parse(message.content).body.products : null
+
+  function fetchImage(meta: any) {
+    if ('product_image' in meta) {
+      if (meta.product_image.length && meta.product_image[0] === '{') {
+        return JSON.parse(meta.product_image).url
+      }
+    }
+
+    return ''
+  }
+
+  console.log(products)
+
   return (
     <div
       className={cn('group relative mb-4 flex items-start md:-ml-12')}
@@ -71,8 +124,29 @@ export function ChatMessage({ message, ...props }: ChatMessageProps) {
             }
           }}
         >
-          {message.content}
+          {message.role !== 'user'
+            ? JSON.parse(message.content).body.response2
+            : message.content}
         </MemoizedReactMarkdown>
+        {/* Products go here */}
+
+        <div className="flex justify-start items-start gap-2">
+          {products &&
+            products.map((prod: any, i: any) => (
+              <div className="border rounded-md">
+                <img
+                  className="w-[100px] h-[100px] rounded-md bg-gray-100 dark:bg-gray-800"
+                  src={fetchImage(prod.metadata)}
+                  alt=""
+                />
+                <p>{prod.metadata.brand_name}</p>
+                <p>
+                  {prod.metadata.currency}:{prod.metadata.price}
+                </p>
+              </div>
+            ))}
+        </div>
+
         <ChatMessageActions message={message} />
       </div>
     </div>
